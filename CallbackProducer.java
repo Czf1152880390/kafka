9324d9563b19f4e24603ade2c7519245208052e0
fu1152880390 <fu199112162511>
2020-07-15 13:53:42 +0800

    kakfa

---
+++ CallbackProducer.java
@@ -0,0 +1,39 @@
+package com.tmsb.kafka;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+/**
+ * @author Caizf
+ * @date 2020/7/13 -14:03
+ */
+public class CallbackProducer {
+    public static void main(String[] args) throws Exception {
+        //创建配置信息
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"node07:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        //创建生产者对象
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+        //发送数据
+        for (int i = 0; i < 30; i++) {
+
+            producer.send(new ProducerRecord<>("TEST",0,"atguigu","atguigy---"+i),
+                    (recordMetadata, e) -> {
+                if(e ==null){
+                    System.out.println(recordMetadata.partition()
+                            +"-----------"+recordMetadata.offset());
+                }else{
+                    e.printStackTrace();
+                }
+            });
+        }
+        //关闭资源
+        producer.close();
+    }
+
+
+}
+
---
+++ Producer.java
@@ -0,0 +1,39 @@
+package com.tmsb.kafka;
+
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author Caizf
+ * @date 2020/7/13 -11:27
+ */
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        //kafka集群,broker-list
+        props.put("bootstrap.servers", "node07:9092");
+        props.put("acks", "all");
+        //重试次数
+        props.put("retries", "1");
+        //批次大小
+        props.put("batch.size", "16384");
+        //等待时间
+        props.put("linger,ms", "1");
+        //RecordAccumulator 缓冲区大小
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 30; i++) {
+
+            producer.send(new ProducerRecord<>("TEST", "atguigu---"+i));
+        }
+        producer.close();
+
+    }
+
+}
